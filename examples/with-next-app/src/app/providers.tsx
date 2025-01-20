b6ede9852886201bb9ccf8666420aa36b9074863
fleksin <hezhoufan@gmail.com>
2025-01-20 10:40:51 +0800

    example with next app

--- examples/with-next-app/src/app/providers.tsx
+++ examples/with-next-app/src/app/providers.tsx
@@ -3,9 +3,31 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { TomoEVMKitProvider, getDefaultConfig, ConnectButton } from '@tomo-inc/tomo-evm-kit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
 
-import { config } from '../wagmi';
+export const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: true,
+  clientId: 'your client id here'
+});
+
 
 const queryClient = new QueryClient();
 
@@ -13,7 +35,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <TomoEVMKitProvider>{children}</TomoEVMKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
