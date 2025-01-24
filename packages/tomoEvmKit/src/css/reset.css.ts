8d64b727b14fb2fbed4c37a54f7e297462a9625b
fleksin <hezhoufan@gmail.com>
2025-01-24 16:19:42 +0800

    try import top level

--- packages/tomoEvmKit/src/css/reset.css.ts
+++ packages/tomoEvmKit/src/css/reset.css.ts
@@ -1,4 +1,4 @@
-import { style, globalFontFace } from '@vanilla-extract/css';
+import { style, globalFontFace, globalStyle } from '@vanilla-extract/css';
 
 export const base = style({
   border: 0,
@@ -11,6 +11,9 @@ export const base = style({
   verticalAlign: 'baseline',
   WebkitTapHighlightColor: 'transparent',
 });
+
+globalStyle('@import url("https://at.alicdn.com/t/c/font_4714049_v2ep5icmokp.css")', {});
+globalStyle('@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap")', {});
 
 globalFontFace('SwitzerBold', {
   src: `url('https://d13t1x9bdoguib.cloudfront.net/static/fonts/Switzer-Semibold.ttf') format('truetype')`,
